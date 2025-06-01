609615b6de1775b2fc6f9abeea5b9507b35e422e
Acer <vnguyen132az@gmail.com>
2025-06-01 13:52:15 +0700

    refactor: format code

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -20,6 +20,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         }
       })
   );
+
   return (
     <ThemeProvider attribute='class' defaultTheme='light' enableSystem>
       <QueryClientProvider client={queryClient}>
